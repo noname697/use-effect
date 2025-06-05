cecca445b4e10e1a97545ff83388105d4b98ae80
Arthur Manenti <arthur.manenti.videira@gmail.com>
2025-06-05 13:24:16 -0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   const [racas, setRacas] = useState([]);
 
-  
+  useEffect(() => {
+    fetch("http://localhost:8080/doguinhos")
+      .then((response) => response.json())
+      .then((data) => {
+        //setRacas(data)
+      });
+  });
 
   return (
     <div className="App">
@@ -12,7 +18,9 @@ function App() {
         <h1>Bem vindo aos doguinhos</h1>
         <h4>Confira abaixo uma lista de raças dos doguinhos</h4>
         <ul>
-          {racas.map(raca => <li key={raca.nome}>{raca.nome}</li>)}
+          {racas.map((raca) => (
+            <li key={raca.nome}>{raca.nome}</li>
+          ))}
           <li>???</li>
         </ul>
       </header>
